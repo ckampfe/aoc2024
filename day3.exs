a9de6d64b29d185e4da995c3c80fcbfa4b2cfd67
Clark Kampfe <clark.kampfe@gmail.com>
2024-12-03 00:21:48 -0600

    day3

---
+++ day3.exs
@@ -0,0 +1,64 @@
+#!/usr/bin/env elixir
+
+defmodule Common do
+  def read_and_parse_inputs do
+    "inputs/day3_1.txt"
+    |> File.read!()
+    |> String.trim()
+  end
+
+  def mul_expr do
+    ~r/mul\((?<left>\d+),(?<right>\d+)\)/
+  end
+
+  def multiply_and_sum_matches(matches) do
+    Enum.reduce(matches, 0, fn [_, left, right], acc ->
+      {left, _} = Integer.parse(left)
+      {right, _} = Integer.parse(right)
+      left * right + acc
+    end)
+  end
+end
+
+defmodule Part1 do
+  def main do
+    s = Common.read_and_parse_inputs()
+    matches = Regex.scan(Common.mul_expr(), s)
+    Common.multiply_and_sum_matches(matches)
+  end
+end
+
+(Part1.main() == 174_960_292) |> IO.inspect()
+
+defmodule Part2 do
+  def main do
+    s = Common.read_and_parse_inputs()
+    enabled = parse(s)
+    matches = Regex.scan(Common.mul_expr(), enabled)
+    Common.multiply_and_sum_matches(matches)
+  end
+
+  def parse(s) do
+    parse(s, :enabled, <<>>)
+  end
+
+  def parse(<<>>, _status, acc), do: acc
+
+  def parse(<<"do()", rest::binary>>, _status, acc) do
+    parse(rest, :enabled, acc)
+  end
+
+  def parse(<<"don't()", rest::binary>>, _status, acc) do
+    parse(rest, :disabled, acc)
+  end
+
+  def parse(<<c::binary-1, rest::binary>>, :enabled, acc) do
+    parse(rest, :enabled, acc <> c)
+  end
+
+  def parse(<<_c::binary-1, rest::binary>>, :disabled, acc) do
+    parse(rest, :disabled, acc)
+  end
+end
+
+(Part2.main() == 56_275_602) |> IO.inspect()
